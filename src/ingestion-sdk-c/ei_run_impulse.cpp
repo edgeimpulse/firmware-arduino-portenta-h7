af8752344c1fc3fd94b21b9921d22b4f79f33e91
Edge Impulse Firmware Uploader <149399868+edgeimpulse-firmware-uploader[bot]@users.noreply.github.com>
2026-01-28 13:04:45 +0000

    Firmware and SDK release 28 Jan 2026
    SDK version: v1.83.0

--- src/ingestion-sdk-c/ei_run_impulse.cpp
+++ src/ingestion-sdk-c/ei_run_impulse.cpp
@@ -34,6 +34,7 @@
 
 /* Include ----------------------------------------------------------------- */
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
+#include "edge-impulse-sdk/classifier/ei_print_results.h"
 #include "edge-impulse-sdk/dsp/numpy.hpp"
 #include "ei_device_portenta.h"
 #include "ei_microphone.h"
@@ -115,7 +116,7 @@ void run_nn(bool debug, int delay_ms, bool use_max_baudrate) {
             break;
         }
 
-        display_results(&ei_default_impulse, &result);
+        ei_print_results(&ei_default_impulse, &result);
 
         while (ei_get_serial_available() > 0) {
             if (ei_get_serial_byte() == 'b') {
@@ -167,7 +168,7 @@ void run_nn_continuous(bool debug)
         }
 
         if (++print_results >= (EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW >> 1)) {
-            display_results(&ei_default_impulse, &result);
+            ei_print_results(&ei_default_impulse, &result);
             print_results = 0;
         }
 
@@ -258,7 +259,7 @@ void run_nn(bool debug, int delay_ms, bool use_max_baudrate) {
             ei_printf("\r\n");
         }
 
-        display_results(&ei_default_impulse, &result);
+        ei_print_results(&ei_default_impulse, &result);
 
         if (debug) {
             ei_printf("End output\n");
